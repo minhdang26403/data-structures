42dd55c290d20ea2eb83119098096371b719a80a
minhdang26403 <truongminhdang26403@gmail.com>
2022-07-30 12:12:24 +0700

    Implement BST

---
+++ include/bst.h
@@ -0,0 +1,190 @@
+#ifndef BST_H_
+#define BST_H_
+
+namespace ds {
+template<typename Type>
+struct Node {
+  Type value_;
+  Node* parent_;
+  Node* left_;
+  Node* right_;
+
+  Node(const Type& value, Node* parent=nullptr, Node* left=nullptr, Node *right=nullptr)
+    : value_(value), parent_(parent), left_(left), right_(right) {}
+};
+
+template<typename Type>
+class BST {
+ public:
+  BST() = default;
+  ~BST() {
+    DestructTree(root_);
+  }
+
+  BST(const BST& other) = delete;
+  BST& operator=(const BST& other) = delete;
+
+  /**
+   * Searchs the BST for a value
+   * @param value the value to search
+   * @return the pointer to the tree node containing the value
+   */
+  Node* Search(const Type& value) {
+    Node* node = root_;
+    while (node != nullptr && node->value_ != value) {
+      if (value < node->value_) {
+        node = node->left_;
+      } else {
+        node = node->right_;
+      }
+    }
+    return node;
+  }
+
+  /**
+   * Finds the the minimum value of the subtree rooted at a node
+   * @param node the root of that subtree
+   * @return the pointer to the tree node containing the minimum value
+   */
+  Node *Minimum(Node *node) {
+    while (node != nullptr && node->left_ != nullptr) {
+      node = node->left_;
+    }
+    return node;
+  }
+
+  /**
+   * Finds the the maximum value of the subtree rooted at a node
+   * @param node the root of that subtree
+   * @return the pointer to the tree node containing the maximum value
+   */
+  Node *Maximum(Node *node) {
+    while (node != nullptr && node->right_ != nullptr) {
+      node = node->right_;
+    }
+    return node;
+  }
+
+  /**
+   * Finds the successor (next greater) of a node
+   * @param node the node to find its successor
+   * @return a pointer to the successor node
+   */
+  Node* Successor(Node* node) {
+    if (node->right_ != nullptr) {
+      return Minimum(x->right_);
+    }
+    while (node->parent_ != nullptr && node != node->parent_->right_) {
+      node = node->parent_;
+    }
+    return node->parent_;
+  }
+
+  /**
+   * Finds the predecessor of a node
+   * @param node the node to find its predecessor
+   * @return a pointer to the predecessor node
+   */
+  Node* Predecessor(Node* node) {
+    if (node->left_ != nullptr) {
+      return Maximum(x->left_);
+    }
+    while (node->parent_ != nullptr && node != node->parent_->left_) {
+      node = node->parent_;
+    }
+    return node->parent_;
+  }
+
+  /**
+   * Inserts a new element into the binary search tree
+   * @param value the value of the element to insert
+   */
+  void Insert(const Type& value) {
+    auto new_node = new Node(value);
+    Node* parent = nullptr;
+    Node* cur = root_;
+    while (cur != nullptr) {
+      parent = cur;
+      if (value < cur->value_) {
+        cur = cur->left_;
+      } else {
+        cur = cur->right_;
+      }
+    }
+    new_node->parent_ = parent;
+    if (parent == nullptr) {
+      root_ = new_node;
+    } else if (value < parent->value_) {
+      parent->left_ = new_node;
+    } else {
+      parent->right_ = new_node;
+    }
+  }
+
+  /**
+   * Deletes a value from the BST
+   * @param value the value to delete
+   * @return true if deletion succeeds; otherwise, false
+   */
+  bool Delete(const Type& value) {
+    Node *node = Search(value);
+    if (node == nullptr) {
+      return false;
+    }
+    if (node->left_ == nullptr) {
+      Transplant(node, node->right_);
+    } else if (node->right_ == nullptr) {
+      Transplant(node, node->left_);
+    } else {
+      Node *successor = Minimum(node->right_);
+      if (successor->parent_ != node) {
+        Transplant(successor, successor->right_);
+        successor->right_ = node->right_;
+        successor->right_->parent_ = successor;
+      }
+      Transplant(node, successor);
+      successor->left_ = node->left_;
+      successor->left_->parent_ = successor;
+    }
+    delete node;
+    return true;
+  }
+
+ private:
+  /**
+   * A helper function that replaces one subtree with another subtree
+   * @param u subtree to be replaced
+   * @param v subtree to replace
+   */
+  void Transplant(Node* u, Node* v) {
+    if (u->parent_ == nullptr) {
+      root_ = v;
+    } else if (u->parent_->left_ == u) {
+      u->parent_->left_ = v;
+    } else {
+      u->parent_->right_ = v;
+    }
+    if (v != nullptr) {
+      v->parent_ = u->parent_;
+    }
+  }
+
+  /**
+   * Recursively destroy the BST
+   * @param root the root of the tree
+   */
+  void DestructTree(Node* root) {
+    if (root == nullptr) {
+      return;
+    }
+    DestructTree(root->left_);
+    DestructTree(root->right_);
+    delete root;
+  }
+  // The root of the tree
+  Node* root_{};
+};
+
+} // namespace ds
+
+#endif // BST_H_
