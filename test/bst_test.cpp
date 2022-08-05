1d9a30b9c773af40ac0fe6b9076d8dbf7ac5c3cf
minhdang26403 <truongminhdang26403@gmail.com>
2022-08-05 15:02:03 +0700

    Add more test cases for BST

--- test/bst_test.cpp
+++ test/bst_test.cpp
@@ -8,45 +8,38 @@
 
 void TestInsert() {
   std::cout << "----------Test Insert----------\n";
-  ds::BST<int> tree;
-  for (int i = 0; i < 10; ++i) {
-    tree.Insert(i + 1);
-  }
-  assert(tree.Search(2) != nullptr);
-  assert(tree.Search(16) == nullptr);
-  assert(tree.Minimum(tree.GetRoot())->value_ == 1);
+  ds::Vector<int> data = {83,86,77,15,93,35,11,92,49,21};
+  ds::BST<int> tree(data);
+  assert(tree.GetRoot()->value_ == 83);
+  assert(tree.GetRoot()->left_->value_ == 77);
+  assert(tree.GetRoot()->right_->value_ == 86);
+  assert(tree.Search(93)->left_->value_ == 92);
+  assert(tree.Search(92)->parent_->value_ == 93);
+  assert(tree.Search(21)->parent_->value_ == 35);
+  assert(tree.Search(49)->parent_->value_ == 35);
+  assert(tree.Search(15)->left_->value_ == 11);
+  assert(tree.Search(15)->right_->value_ == 35);
 }
 
 void TestDelete() {
   std::cout << "----------Test Delete----------\n";
-  ds::BST<long> tree;
-  for (int i = 0; i < 15; ++i) {
-    tree.Insert(rand() % 100);
-  }
-  tree.Print();
-  assert(tree.Minimum(tree.GetRoot())->value_ == 15);
-  assert(tree.Search(90) != nullptr);
-  tree.Delete(62);
-  tree.Print();
+  // Initialize the tree by initializer list
+  ds::BST<int> tree = {16,65,62,89,91,73,46,66,41,99};
+  assert(tree.GetRoot()->value_ == 16);
+  tree.Delete(16);
+  assert(tree.GetRoot()->value_ == 65);
+  assert(tree.Minimum(tree.GetRoot())->value_ == 41);
+  assert(tree.Search(73)->left_->value_ == 66);
+  assert(tree.Search(73)->parent_->value_ == 89);
+  assert(tree.Search(91)->left_ == nullptr);
 }
 
 void TestConstructor() {
   std::cout << "----------Test Constructor----------\n";
-  ds::BST<int> tree1;
-  for (int i = 0; i < 10; ++i) {
-    tree1.Insert(rand() % 100);
-  }
-  std::cout << "Tree 1: ";
-  tree1.Print();
+  ds::BST<int> tree1 = {44,64,29,89,25,36,73,32,63,93};
   ds::BST<int> tree2(tree1);
-  std::cout << "Tree 2: ";
-  tree2.Print();
   assert(tree1.GetRoot() != tree2.GetRoot());
   tree1.Delete(36);
-  std::cout << "Tree 1: ";
-  tree1.Print();
-  std::cout << "Tree 2: ";
-  tree2.Print();
   assert(tree1.Search(36) == nullptr);
   assert(tree2.Search(36) != nullptr);
   tree2.Insert(30);
@@ -58,18 +51,16 @@ void TestConstructor() {
   tree3.Print();
   assert(tree1.GetRoot() == nullptr);
   assert(tree3.GetRoot() != nullptr);
-  assert(tree3.Search(11) != nullptr);
+  assert(tree3.Search(44) != nullptr);
 }
 
 void TestCustomConstructor() {
   ds::BST<std::string> tree1 = {"abc", "esi", "def", "sv", "nvch"};
   assert(tree1.GetRoot() != nullptr);
-  tree1.Print();
   assert(tree1.Search("nvch") != nullptr);
   ds::Vector<std::string> data = {"abc", "qwe", "qaz", "kom", "poi"};
   ds::BST<std::string> tree2(data);
   assert(tree2.Search("qaz") != nullptr);
-  tree2.Print();
 }
 
 int main() {
