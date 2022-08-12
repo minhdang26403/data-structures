a2a3dfcebe261cdf38db771695d4ce9539ce4324
minhdang26403 <truongminhdang26403@gmail.com>
2022-08-12 22:06:16 +0700

    Unit tests for graphs and BFS

---
+++ test/undirected_graph_test.cpp
@@ -0,0 +1,26 @@
+#include "undirected_graph.h"
+
+void Test() {
+  using namespace ds;
+  UndirectedGraph<int> graph;
+  Vertex<int>* v1 = graph.AddVertex(1);
+  Vertex<int>* v2 = graph.AddVertex(2);
+  Vertex<int>* v3 = graph.AddVertex(3);
+  Vertex<int>* v4 = graph.AddVertex(4);
+  Vertex<int>* v5 = graph.AddVertex(5);
+  graph.AddEdge(v1, v5);
+  graph.AddEdge(v1, v2);
+  graph.AddEdge(v2, v3);
+  graph.AddEdge(v2, v4);
+  graph.AddEdge(v2, v5);
+  graph.AddEdge(v3, v4);
+  graph.AddEdge(v4, v5);
+
+  graph.Print();
+}
+
+int main() {
+  Test();
+
+  return 0;
+}
